1ea989833a21e5b0c80b0fddf3e2fa7506a8039a
rahulall <rahul.la@gmail.com>
2025-07-07 14:49:46 +0530

    first commit

---
+++ smartSparen/src/main/java/com/example/smartSparen/SmartSparenApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartSparen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSparenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSparenApplication.class, args);
+	}
+
+}
---
+++ smartSparen/src/test/java/com/example/smartSparen/SmartSparenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartSparen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSparenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
