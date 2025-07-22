72ba70b1cbe2415dbe44442838663cfe70db4c5c
aniruddhkurundkar <aniruddhkurundkar10@gmail.com>
2025-07-22 22:46:54 +0530

    modified api for goal

--- smartSparen/src/main/java/com/hackathon/expensemanger/controller/GoalsController.java
+++ smartSparen/src/main/java/com/hackathon/expensemanger/controller/GoalsController.java
@@ -1,5 +1,6 @@
 package com.hackathon.expensemanger.controller;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
 import com.hackathon.expensemanger.dao.GoalDao;
 import com.hackathon.expensemanger.entity.Goal;
 import com.hackathon.expensemanger.util.HttpResponse;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +25,12 @@ public class GoalsController {
     private GoalDao goalDao;
 
     @GetMapping("/fetchGoals")
-    public List<Goal> fetchGoals(){
+    public List<Goal> fetchGoals(@RequestBody Goal goal){
         HttpResponse<Goal> httpResponse = new HttpResponse<>();
         logger.info("Updating Goals object");
         List<Goal> goals = null;
         try {
-            goals = goalDao.findAll();
+            goals = goalDao.findById(goal.getGoalAccountId()).stream().toList();
             logger.info("End of method fetching goals");
         } catch (Exception e) {
             logger.error("exception occurred while updating goals : ", e);
@@ -40,6 +42,8 @@ public class GoalsController {
         HttpResponse<Goal> httpResponse = new HttpResponse<>();
         logger.info("Adding Goals object");
         try {
+            Integer goalAchievedAmountInPercent = (goal.getGoalAchievedAmount().divide(goal.getGoalAmount())).multiply(new BigDecimal(100)).intValue();
+            goal.setGoalPercentAchieved(goalAchievedAmountInPercent);
             goalDao.save(goal);
             httpResponse.setStatus(SUCCESS);
             httpResponse.setMessage(MSG_FOR_SUCCESSFUL_INSERTION);
@@ -64,7 +68,8 @@ public class GoalsController {
             goalObj.setGoalAccountId(goal.getGoalAccountId());
             goalObj.setGoalAmount(goal.getGoalAmount());
             goalObj.setGoalType(goal.getGoalType());
-            goalObj.setGoalPercentAchieved(goal.getGoalPercentAchieved());
+            Integer goalAchievedAmountInPercent = (goal.getGoalAchievedAmount().divide(goal.getGoalAmount())).multiply(new BigDecimal(100)).intValue();
+            goal.setGoalPercentAchieved(goalAchievedAmountInPercent);
             goalObj.setGoalAchievedAmount(goal.getGoalAchievedAmount());
             goalDao.save(goalObj);
             httpResponse.setStatus(SUCCESS);
