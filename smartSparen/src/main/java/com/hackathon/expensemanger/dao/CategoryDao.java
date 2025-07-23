1b61941b81d7b29e60ca8dba3a3cfc2cee3298e8
rahulall <rahul.la@gmail.com>
2025-07-23 21:27:04 +0530

    Added code for GET API CategoryDetails

--- smartSparen/src/main/java/com/hackathon/expensemanger/dao/CategoryDao.java
+++ smartSparen/src/main/java/com/hackathon/expensemanger/dao/CategoryDao.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface CategoryDao extends JpaRepository<Category, Long> {
 
-            @Query(name = "Category.findByCategoryMonthlyData", value = "SELECT e.user_id, c.category_name, EXTRACT(YEAR FROM e.transaction_date) AS year, " +
+           /* @Query(name = "Category.findByCategoryMonthlyData", value = "SELECT e.user_id, c.category_name, EXTRACT(YEAR FROM e.transaction_date) AS year, " +
                     "TO_CHAR(e.transaction_date, 'Month') AS month, SUM(e.amount) AS total_expense " +
                     "FROM smart_sparen.expenses e JOIN "+
                     "smart_sparen.category c ON e.category_id = c.category_id "+
@@ -20,5 +20,33 @@ public interface CategoryDao extends JpaRepository<Category, Long> {
                     "AND e.transaction_date < DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month' "+
                     "GROUP BY e.user_id, c.category_name, year, month "+
         "ORDER BY e.user_id, year, month, c.category_name", nativeQuery = true)
-            public List findByCategoryMonthlyData();
+            public List findByCategoryMonthlyData();*/
+
+    @Query(name = "Category.findByCategoryMonthlyData", value = "SELECT user_id, category_name, TO_CHAR(month_start, 'YYYY-MM') AS month, total_expense, "+
+            "CASE "+
+            "WHEN previous_month_expense IS NULL THEN 'no data' "+
+            "WHEN total_expense > previous_month_expense THEN 'more' "+
+            "WHEN total_expense < previous_month_expense THEN 'low' "+
+            "ELSE 'same' "+
+            "END AS insight "+
+            "FROM ( "+
+            "SELECT "+
+            "e.user_id, c.category_name, DATE_TRUNC('month', e.transaction_date) AS month_start, "+
+            "SUM(e.amount) AS total_expense, LAG(SUM(e.amount)) OVER (PARTITION BY e.user_id, c.category_name "+
+            "ORDER BY DATE_TRUNC('month', e.transaction_date)) AS previous_month_expense "+
+            "FROM "+
+            "smart_sparen.expenses e "+
+            "JOIN "+
+            "smart_sparen.category c ON e.category_id = c.category_id "+
+            "WHERE "+
+            "DATE_TRUNC('month', e.transaction_date) >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 month' "+
+            "AND DATE_TRUNC('month', e.transaction_date) <= DATE_TRUNC('month', CURRENT_DATE) "+
+            "GROUP BY "+
+            "e.user_id, "+
+            "c.category_name, "+
+            "DATE_TRUNC('month', e.transaction_date) "+
+            ") AS sub ORDER BY user_id,category_name, month_start", nativeQuery = true)
+    public List findByCategoryMonthlyData();
+
+
 }
